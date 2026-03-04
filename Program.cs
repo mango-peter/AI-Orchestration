70a2ff6e3e506e70bc600682fbefbbe5a6122753
peter@ledovia.com <peter@ledovia.com>
2026-03-04 13:06:08 -0500

    Added docker file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World again!");
+
+
+Console.WriteLine(args.Length);
+
+
+var counter = 0;
+var max = 20;// args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+
+while (max is -1 || counter < max)
+{
+    Console.WriteLine($"Counter: {++counter}");
+
+    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+}
